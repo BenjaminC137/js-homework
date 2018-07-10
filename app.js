63d2325fec981bb9196abb480d58c4d1947f06f4
Benjamin <benjaminsalasek@Athena.local>
2018-07-10 15:01:19 -0500

    initial

---
+++ app.js
@@ -0,0 +1,58 @@
+//function displayLarger(a, b){
+//	if(a > b){
+//		message = "a";
+//		alert(message);
+//	}
+//	else{
+//		message = "b";
+//		alert(message);
+//	}
+//}
+//
+//largest = largest.sort((a, b, c, d, e) => (largest));
+//	alert(largest);
+//
+//var entry;
+//var row = "";
+//var currentNumber = 0;
+//
+//function diamond(entry){
+//	if(!entry){
+//		entry = 5;
+//	}
+//	for(i = 1; i <= entry; i ++){
+//		currentNumber ++;
+//		row = row.concat(currentNumber);
+//		console.log(row);
+//	}
+//
+//	for(i = entry; i > 1; i --){
+//		line --;
+//		console.log(currentNumber);
+//	}
+//}
+//
+//var a = {};
+//
+//var person = {
+//name: "Bob",
+//age: 29,
+//gender: "neutral",
+//}
+//
+//var employee = {
+//Name: "Steve",
+//Title: "chief",
+//IDnumber: 04,
+//}
+
+
+//function currentDate () {
+//	var myDate = new Date (Date.now());
+//	var month = myDate.getMonth();
+//	var year = myDate.getFullYear();
+//	var day = myDate.getDate();
+//	console.log(month + "/" + day + "/" + year);
+//	return(month + "/" + day + "/" + year);
+//}
+
