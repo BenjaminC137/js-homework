ceb6907c41d0830298a9cee1d334a4eeca20c7c2
Benjamin <benjaminsalasek@Athena.local>
2018-07-13 11:08:54 -0500

    weather app works

--- weather.js
+++ weather.js
@@ -1,77 +1,45 @@
-var cityAndState = "Chicago, IL";
-var resultsText;
-var forcastResult;
-var jsonDataTenDay;
-var weatherAPI = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + cityAndState + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+//var cityAndState = "Chicago, IL";
 
-//$.getJSON(weatherAPI, (jsonData) => { console.log(jsonData.query.results.channel.description); })
+var forcastResult, jsonDataTenDay, resultsText;
 
-$.getJSON(weatherAPI, (jsonData) => { $("place").html(jsonData.query.results.channel.description); })
+var locationSubmission;
 
-$.getJSON(weatherAPI, (jsonData) => { $("weather").html(jsonData.query.results.channel.item.description); })
+var weatherAPI;
 
+var cityEntry;
+var stateEntry;
 
-//$.getJSON(weatherAPI, (jsonData) => { $("p").text(jsonData.query.results.channel.item.forecast); })
-
-//$.getJSON(weatherAPI, (jsonData) => { $.each(jsonData.query.results.channel.item, function	(key, value){$("p").append(key + ": " + value + "<br>")}); })
-
-//$.getJSON(weatherAPI, (jsonData) => { $.each(jsonData.query.results.channel.item.forecast, function	(index, value){
-//	$.each(this.items, function(){ console.log(this.value)
-//		});
-//	 });
-//});
-
-//{ $.each(jsonData, function())
-//	forcastResult = jsonDataTenDay.query.results.channel.item.forecast;
+$("weather").toggle();
+$("place").toggle();
 
 
+function getFormvalue(){
+	cityEntry = document.getElementsByName("cityEntry")[0];
+	stateEntry = document.getElementsByName("stateEntry")[0];
 
-//
-//function callback1(jsonDataCity){
-//	console.log("callback1 running");
-//	console.log(jsonDataCity.query.results.channel.description);
-//}
-//
-//function callback2(jsonDataTenDay){
-//	console.log("callback2 running");
-//	forcastResult = jsonDataTenDay.query.results.channel.item.forecast;
-//	$("p").text("weather will go here.");
-//}
+	showLocalWeather();
+	return false;
+}
+
+function showLocalWeather(){
+		locationSubmission = (cityEntry.value + "," + stateEntry.value);
+		console.log(locationSubmission);
+
+	weatherAPI = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + locationSubmission + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+	console.log(weatherAPI);
+
+	$.getJSON(weatherAPI, (jsonData) => {
+		$("place").html(jsonData.query.results.channel.description); })
+		$("place").fadeIn(1000);
+
+	$.getJSON(weatherAPI, (jsonData) => {
+		$("weather").html(jsonData.query.results.channel.item.description);
+		$("weather").fadeIn(1000);
+	});
 
 
 
-//$.getJSON(weatherAPI, callback1);
-//
-////$.getJSON(weatherAPI, function(jsonDataTenDay){ console.log(jsonDataTenDay.query.results.channel.item.forecast); })
-//
-//$.getJSON(weatherAPI, callback2);
-//
-//console.log("Putting forecast into inner html");
-//resultsText.innerHTML = jsonDataTenDay;
-
-
-//$.getJSON(weatherAPI, function(jsonData){ console.log(jsonData); })
-
-            function getFormvalue(){
-                var fname = document.getElementsByName("fname")[0];
-                var lname = document.getElementsByName("lname")[0];
-                //This is what the assignment asks for:
-                console.log(fname.value);
-                console.log(lname.value);
-
-                //How do people use this?  Form validation!
-                var isValid = true;
-                if(fname.value.length === 0){
-                    fname.style.boxShadow = "1px 1px 1px red";
-                    isValid = false;
-                }
-
-                if(lname.value.length === 0){
-                    lname.style.boxShadow = "1px 1px 1px red";
-                    isValid = false;
-                }
-                return isValid;;
-            }
+}
 
 
 //https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
